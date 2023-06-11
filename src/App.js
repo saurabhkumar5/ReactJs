222b65a73d8338185531dc32085cf660a83a8046
saurabh kumar <saurabhkumar9493@gmail.com>
2023-06-12 00:11:34 +0530

    if-else and ternary operator

--- src/App.js
+++ src/App.js
@@ -3,14 +3,25 @@ import Header from './components/Header';
 import Movie from './components/Movie';
 import data from './data';
 function App(){
+    let login = false;
+    // if(login==false){
+    //   return <h1>ja mai tujhe nhi dikhega</h1>
+    // }
+
+//jsx k andar direct termnary operator or if else use nhi kar sakte h
+
   return (
     <div className='App'>
+      {
+        login == true ? <h1 style={{color:"black"}}>ja bhai yha se</h1>:<h1 style={{color:"black"}}>ja dekh le</h1>
+      }
       <Header/>
       <div className='main'>
         {
-        data.map((element)=>{
+        data.map((element,index)=>{
           return (
             <Movie
+            key = {index}
              title = {element.Title}
              year = {element.Year}
              img = {element.Poster}         
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,11 +1,16 @@
 
 
-const Movie = (props) => {
+// const Movie = (props) => { 
+    const Movie = ({img,title,year}) => { //object destructring
   return (
     <div className="movie">
-        <img src= {props.img}/>
+        {/* <img src= {props.img}/>
         <p>Star Wars: {props.title}</p>
-        <p>Year: {props.year}</p>
+        <p>Year: {props.year}</p> */}
+        <img src= {img}/>
+        <p>Star Wars: {title}</p>
+        <p>Year: {year}</p>
+
     </div>
   )
 }
