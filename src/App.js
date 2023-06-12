71a21d75e2815186a93dbf1fe1ad33489e223b30
saurabh kumar <saurabhkumar9493@gmail.com>
2023-06-12 17:33:50 +0530

    Record keeping project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Header from "./components/Header";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+import AddIcon from "@mui/icons-material/Add";
+import { useState } from "react";
+import Fields from "./components/Fields";
 function App() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [data,setData] = useState([])
+
+  const addData = ()=>{
+    // setData({
+    //   // name: name,
+    //   // email: email
+    //   //or
+    //   name,email
+    // })
+    setData([...data, {name,email}]);
+    setName("");
+    setEmail("");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      {/* form */}
+      <div className="form">
+        <Stack spacing={2} direction="row">
+          <TextField
+            value={name}
+            onChange={(event)=>setName(event.target.value)}
+            id="outlined-basic"
+            label="name"
+            variant="outlined"
+          />
+          <TextField
+            value={email}
+            onChange={(event)=>setEmail(event.target.value)}
+            id="outlined-basic"
+            label="email"
+            variant="outlined"
+          />
+          <Button onClick={addData} color="success" variant="contained">
+            <AddIcon />
+          </Button>
+        </Stack>
+      </div>
+
+      {/* data */}
+      <div className="data">
+        <div className="data_val">
+          <h4>Name</h4>
+          <h4>Email</h4>
+          <h4>Remove</h4>
+        </div>
+        {
+          data.map((element,index)=>{
+            return (
+              <Fields key={index} name = {element.name} email = {element.email} index = {index}/>
+            )
+          })
+        }
+
+      </div>
     </div>
   );
 }
